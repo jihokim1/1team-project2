4ac520a4c231de1aae209571f58be5f24e981df0
seoyoony <yoonyoon0403@naver.com>
2025-03-24 18:29:23 +0900

    allfile

---
+++ test_team/android/app/src/main/kotlin/com/example/test_team/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_team
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test_team/lib/view/member_04.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Member04 extends StatelessWidget {
+  const Member04({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
